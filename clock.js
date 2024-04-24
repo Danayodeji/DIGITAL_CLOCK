12accaa93477bcae746d1936ab3289960ddf22e5
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2024-04-24 13:25:39 +0100

    show digital clock function

--- clock.js
+++ clock.js
@@ -5,3 +5,6 @@ let getMinutesTime = document.getElementById("min")
 let getSecondsTime = document.getElementById("sec")
  
 // STEP 2: create a that show live time and assign hour, minutes,and seconds to new variables
+const showDigitalClock = () => {
+    
+};
