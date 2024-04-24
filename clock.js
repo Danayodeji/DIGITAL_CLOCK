4f94cf7cca85c7e3f3aba957f8dd60f5efbc11b4
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2024-04-24 13:50:26 +0100

    step 4

--- clock.js
+++ clock.js
@@ -12,8 +12,10 @@ const showDigitalClock = () => {
     let getMinutes = getDate.getMinutes().toString().padStart(2,"0");
     let getSeconds = getDate.getSeconds().toString().padStart(2,"0");
 
-// STEP 3 Display new content on the HTML
+// STEP 3:  Display new content on the HTML
     getHourTime.textContent = getHour
     getMinutesTime.textContent = getMinutes
     getSecondsTime.textContent = getSeconds
 };
+
+// STEP 4: The following code make the counting live 
